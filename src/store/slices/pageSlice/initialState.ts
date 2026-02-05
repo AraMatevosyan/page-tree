9536f03f3c65cba5b7716376794174be8e513e8a
Ara Matevosyan <matevossyan77@gmail.com>
2026-02-05 15:27:21 +0400

    fixes

--- src/store/slices/pageSlice/initialState.ts
+++ src/store/slices/pageSlice/initialState.ts
@@ -7,42 +7,6 @@ export const initialState: PagesState = {
     title: 'Main',
     id: createId(),
     pageBlocks: [],
-    children: [
-      {
-        title: 'Page',
-        id: createId(),
-        pageBlocks: [{ title: 'Block', id: createId() }],
-        children: [],
-      },
-      {
-        title: 'Page',
-        id: createId(),
-        pageBlocks: [],
-        children: [],
-      },
-      {
-        title: 'Page',
-        id: createId(),
-        pageBlocks: [
-          { title: 'Block 1', id: createId() },
-          { title: 'Block 2', id: createId() },
-          { title: 'Block 3', id: createId() },
-        ],
-        children: [],
-      },
-      {
-        title: 'Page',
-        id: createId(),
-        pageBlocks: [],
-        children: [
-          {
-            title: 'Page',
-            id: createId(),
-            pageBlocks: [],
-            children: [],
-          },
-        ],
-      },
-    ],
+    children: [],
   },
 };
